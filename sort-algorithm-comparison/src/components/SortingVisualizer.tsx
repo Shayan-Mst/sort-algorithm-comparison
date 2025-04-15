e25dd474efadc8ab83dc3b7a8d00a9d482b930f8
Shayan-Mst <shnbackup007@gmail.com>
2025-04-15 20:58:09 +0330

    replace diffrent algorithm added

--- sort-algorithm-comparison/src/components/SortingVisualizer.tsx
+++ sort-algorithm-comparison/src/components/SortingVisualizer.tsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import Bars from "./Bar";
 import Controls from "./Control";
 import { bubbleSort } from "./../algorithms/bubbleSort";
+import { quickSort } from "../algorithms/quickSort";
+import { heapSort } from "../algorithms/heapSort";
+import { selectionSort } from "../algorithms/selectionSort";
+import { mergeSort } from "../algorithms/mergeSort";
 import generateRandomArray from "./../utils/generateArray";
 
 
@@ -9,16 +13,25 @@ const SortingVisualizer = () => {
     const [array, setArray] = useState<number[]>([]);
     const [highlighted, setHighlighted] = useState<number[]>([]);
     const [customInput, setCustomInput] = useState("");
-    const [sortAlg,setSortAlg] = useState('bubble');
+    const [sortAlg,setSortAlg] = useState('Bubble');
+    const [flag,setFlag] = useState("");
   
     const generateRandomArrayFunc = () => {
      
       const randomArray = generateRandomArray(20)
       setArray(randomArray);
     };
+
+    const HandleAlgorithm = (e: any) => {setFlag(e.target.id)}
+  
   
     const handleStart = () => {
-      bubbleSort(array, setArray, setHighlighted);
+      if(flag === "1") bubbleSort(array, setArray, setHighlighted);
+      else if(flag === "2") mergeSort(array, setArray, setHighlighted);
+      else if(flag === "3") quickSort(array, setArray, setHighlighted);
+      else if(flag === "4") heapSort(array, setArray, setHighlighted);
+      else if(flag === "5") selectionSort(array, setArray, setHighlighted);
+      
     };
   
     const handleCustomInput = (val: string) => {
@@ -35,7 +48,20 @@ const SortingVisualizer = () => {
    
     <div className="min-h-screen grid items-end bg-gray-100 p-8 text-center">
     <div className="self-start">
-        <h1 className="text-3xl font-bold text-center text-slate-700"> {sortAlg} Sort Visualizer</h1>
+        <h1 className="text-2xl font-bold text-center text-slate-700 inline"> {sortAlg} Sort Visualizer</h1>
+        <select
+          value={sortAlg}
+         
+          onChange={(e)=>setSortAlg(e.target.value)}
+          className="px-3 mx-2 py-2 rounded bg-gray-700 text-white text-center"
+        >
+          <option onClick={HandleAlgorithm} id="1" value="Bubble">Bubble Sort</option>
+          <option onClick={HandleAlgorithm} id="2" value="Merge">Merge Sort</option>
+          <option onClick={HandleAlgorithm} id="3" value="Quick">Quick Sort</option>
+          <option onClick={HandleAlgorithm} id="4" value="Heap">Heap Sort</option>
+          <option onClick={HandleAlgorithm} id="5" value="Selection">Selection Sort</option>
+        </select>
+
         </div>
       <Bars array={array} highlighted={highlighted} />
       <Controls
